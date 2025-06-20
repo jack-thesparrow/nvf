0c1661d71599eace88aea5c18673d18973e6f5e1
jack-thesparrow <rahultudu4705@gmail.com>
2025-06-20 13:13:16 +0530

    added surround

--- config/core/utility.nix
+++ config/core/utility.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   vim.utility = {
     preview = {
@@ -61,6 +60,25 @@
 
       # Optional extra options passed to smart-splits.setup()
       setupOpts = { };
+    };
+    surround = {
+      enable = true;
+      useVendoredKeybindings = true;
+      setupOpts = {
+        keymaps = {
+          change = "gzr";
+          change_line = "gZR";
+          delete = "gzd";
+          insert = "<C-g>z";
+          insert_line = "<C-g>Z";
+          normal = "gz";
+          normal_cur = "gZ";
+          normal_cur_line = "gZZ";
+          normal_line = "gzz";
+          visual = "gz";
+          visual_line = "gZ";
+        };
+      };
     };
   };
 }
